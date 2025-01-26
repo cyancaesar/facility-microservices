906b054cdb210e3c9b8a382653ee98ad0c55a6cc
cyancaesar <29235530+cyancaesar@users.noreply.github.com>
2025-01-26 19:03:04 +0300

    Tweak Docker Compose file
    Removed Dockerfile so there is no need to have containerized node run
    time. Only have two databases, each for its associated services.
    
    Also, each service has its own environment variables.

--- apps/virtual-facility/src/app.module.ts
+++ apps/virtual-facility/src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BuildingsModule } from './buildings/buildings.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'apps/virtual-facility/.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: +process.env.POSTGRES_PORT,
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      password: process.env.POSTGRES_PASS,
       database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       synchronize: true,
--- apps/workflows-service/src/workflows-service.module.ts
+++ apps/workflows-service/src/workflows-service.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkflowsServiceController } from './workflows-service.controller';
 import { WorkflowsServiceService } from './workflows-service.service';
@@ -6,12 +7,15 @@ import { WorkflowsModule } from './workflows/workflows.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'apps/workflows-service/.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: +process.env.POSTGRES_PORT,
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      password: process.env.POSTGRES_PASS,
       database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       synchronize: true,
